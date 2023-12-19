914f74172fb98ed01b9e83789d85696bef18c53b
haymanot-demis <haymedin21@gmail.com>
2023-12-19 18:54:25 +0300

    Add QuestionResultEntity

---
+++ Domain/Entities/QuestionResultEntity.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class QuestionResultEntity : BaseDomainEntity
+{
+    public double Points { get; set; }
+    public int RejectedAttemptCount { get; set; }
+    public int BestSubmissionTimeSeconds { get; set; }
+}
