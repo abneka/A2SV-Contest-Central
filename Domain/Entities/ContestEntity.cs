1edabd947899aa029ca343bfd58bd9dbc240a835
mie-jejaw <mieraf.jejaw@a2sv.org>
2023-12-19 18:39:37 +0300

    create contest and userType entitie in domain

---
+++ Domain/Entities/ContestEntity.cs
@@ -0,0 +1,23 @@
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class ContestEntity : BaseDomainEntity
+    {
+        public string ContestGlobalId { get; set; } = null!;
+        public string ContestUrl { get; set; } = null!;
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public TimeSpan Duration { get; set; }
+        public DateTime StartTime { get; set; }
+        public TimeSpan RelativeTime { get; set; }
+        public string PreparedBy { get; set; } = string.Empty;
+        public string WebsiteUrl { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Difficulty { get; set; } = string.Empty;
+        public string Kind { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Season { get; set; } = string.Empty;
+    }
+}
