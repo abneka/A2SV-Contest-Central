b13c840d500a87dce9790f700a50a6df507908d3
abesolom.nekahiwot <anekahiwot@gmail.com>
2023-12-19 18:39:20 +0300

    User Entity Added:  - it includes Name, Group, Email, etc.

---
+++ Domain/Entities/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class UserEntity : BaseDomainEntity
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Group { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public string CodeforcesHandle { get; set; } = null!;
+    public string Password { get; set; } = null!; 
+}
