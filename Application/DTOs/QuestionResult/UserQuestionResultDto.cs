70e25d054275a0d43c4f52ef6c019170e8fd844e
haymanot-demis <haymedin21@gmail.com>
2023-12-22 18:40:15 +0300

    UserQuestionResult-Group-Location

---
+++ Application/DTOs/QuestionResult/UserQuestionResultDto.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs.Common;
+using Application.DTOs.User;
+
+namespace Application.DTOs.QuestionResult;
+
+public class UserQuestionResultDto : BaseDto
+{
+    public double Points { get; set; }
+    public int RejectedAttemptCount { get; set; }
+    public int BestSubmissionTimeSeconds { get; set; }
+    public Guid UserId { get; set; }
+    public UserResponseDto User { get; set; } = null!;
+    public Guid QuestionId { get; set; }
+    public QuestionResponseDto Question { get; set; } = null!;
+}
