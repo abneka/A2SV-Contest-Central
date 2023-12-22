dda02175e022ce16118d890dd42e325b96609364
mikeyas-alemu <mikeyas.alemu@a2sv.org>
2023-12-22 18:44:52 +0300

    First Commit

---
+++ Domain/Entities/ContestGroupEntity.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class ContestGroupEntity : BaseDomainEntity
+{
+   
+    public Guid ContestId { get; set; }
+    
+    public int GroupNumber { get; set; }
+}
