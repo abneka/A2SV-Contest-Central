7f38ab896f845f30448a51254b543c98637d49cc
haymanot-demis <haymedin21@gmail.com>
2023-12-22 19:16:30 +0300

    Add TeamEntity

---
+++ Domain/Entities/TeamEntity.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class TeamEntity : BaseDomainEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public Guid GroupId { get; set; }
+    public GroupEntity? Group { get; set; }
+    public List<TeamContestResultEntity> TeamContestResults { get; set; } = new List<TeamContestResultEntity>();
+    public List<TeamQuestionResultEntity> TeamQuestionResults { get; set; } = new List<TeamQuestionResultEntity>();
+}
