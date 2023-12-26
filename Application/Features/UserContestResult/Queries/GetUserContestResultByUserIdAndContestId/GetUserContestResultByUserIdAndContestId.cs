ab41dc6e7cedbf824e73b54743a2ca1f8ab5e3bc
Nahom Habtamu <nahom.habtamu@a2sv.org>
2023-12-26 18:16:43 +0300

    Queries for UserContestResult by different filters

--- Application/Features/UserContestResult/Queries/GetUserContestResultByUserIdAndContestId/GetUserContestResultByUserIdAndContestId.cs
+++ Application/Features/UserContestResult/Queries/GetUserContestResultByUserIdAndContestId/GetUserContestResultByUserIdAndContestId.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace Application.Features.UserContestResult.Queries.GetUserContestResultByUserId;
 
-public class GetUserContestResultByUserId : IRequest<UserContestResultResponseDto>
+public class GetUserContestResultByUserIdAndContestId : IRequest<UserContestResultResponseDto>
 {
     public Guid UserId { get; set; }
     public Guid ContestId { get; set; }
