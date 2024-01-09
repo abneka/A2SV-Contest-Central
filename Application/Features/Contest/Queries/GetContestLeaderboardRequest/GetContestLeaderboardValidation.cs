6e854444d09e1139cbee2fa47ca767a817618452
Nahom Habtamu <nahom.habtamu@a2sv.org>
2024-01-09 16:21:52 +0300

    validation for contest leaderboard

---
+++ Application/Features/Contest/Queries/GetContestLeaderboardRequest/GetContestLeaderboardValidation.cs
@@ -0,0 +1,20 @@
+﻿using Application.Contracts.Persistence;
+using FluentValidation;
+
+namespace Application.Features.Contest.Queries;
+
+public class GetContestLeaderboardValidation : AbstractValidator<GetContestLeaderboardRequest>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetContestLeaderboardValidation(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+
+        RuleFor(req => req.ContestId)
+            .NotEmpty()
+            .WithMessage("Contest URL cannot be empty.")
+            .MustAsync(async (contestId, token) => await _unitOfWork.ContestRepository.Exists(contestId))
+            .WithMessage("Invalid Codeforces contest URL.");
+    }
+}
