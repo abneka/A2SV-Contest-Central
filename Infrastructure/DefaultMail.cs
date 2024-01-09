3fa63acfae44b71c645ca2fcd9ba34c04b4f6b02
Nahom Habtamu <nahom.habtamu@a2sv.org>
2024-01-09 18:31:19 +0300

    fix: update email name

--- Infrastructure/DefaultMail.cs
+++ Infrastructure/DefaultMail.cs
@@ -2,7 +2,7 @@
 
 public class DefaultMail
 {
-  public static string RegistrationSubject = "Wellcome to Our Social Media App";
+  public static string RegistrationSubject = "Welcome to A2SV Contest Central";
     public static string RegistrationBody =
     @"<!DOCTYPE HTML PUBLIC ""-//W3C//DTD XHTML 1.0 Transitional //EN"" ""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"">
 <html xmlns=""http://www.w3.org/1999/xhtml"" xmlns:v=""urn:schemas-microsoft-com:vml"" xmlns:o=""urn:schemas-microsoft-com:office:office"">
